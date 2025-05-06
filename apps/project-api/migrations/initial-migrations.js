5cac5bdb905d5c05e442b11a8c82e7791496f565
Kári Yngvason <kari@hugsmidjan.is>
2025-05-06 11:28:06 +0000

    feat: add base database schema (#4)

--- apps/project-api/migrations/initial-migrations.js
+++ apps/project-api/migrations/initial-migrations.js
@@ -2,21 +2,195 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface) {
-    return await queryInterface.sequelize.query(`
-    BEGIN;
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.createTable(
+        'th_address',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          address: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+        },
+        {
+          transaction: t,
+        },
+      )
 
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+      await queryInterface.createTable(
+        'th_people',
+        {
+          nationalId: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          type: {
+            type: Sequelize.ENUM('person', 'legal_entity'),
+          },
+          residence_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: 'th_address',
+              key: 'id',
+            },
+          },
+        },
+        {
+          transaction: t,
+        },
+      )
 
-    CREATE TABLE IF NOT EXISTS FOO (
-      ID UUID NOT NULL DEFAULT UUID_GENERATE_V4 (),
-      TITLE VARCHAR NOT NULL,
-      SLUG VARCHAR NOT NULL,
-      PRIMARY KEY (ID)
-    );
+      queryInterface.createTable(
+        'tax_return',
+        {
+          year: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+          },
+          person_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'th_people',
+              key: 'nationalId',
+            },
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+        },
+        {
+          transaction: t,
+          primaryKeys: ['year', 'person_id'],
+        },
+      )
 
-    COMMIT;
+      await queryInterface.createTable(
+        'income_types',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+        },
+        {
+          transaction: t,
+        },
+      )
 
-    `)
+      await queryInterface.createTable(
+        'income',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          created: {
+            type: 'TIMESTAMP WITH TIME ZONE',
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            allowNull: false,
+          },
+          modified: {
+            type: 'TIMESTAMP WITH TIME ZONE',
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            allowNull: false,
+          },
+          type: {
+            type: Sequelize.ENUM('prefill', 'submit'),
+            allowNull: false,
+          },
+          income_type_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: 'income_types',
+              key: 'id',
+            },
+          },
+          payer: {
+            type: Sequelize.STRING, // TODO: setja upp sér töflu og vensla?
+            allowNull: false,
+          },
+          value: {
+            type: Sequelize.FLOAT,
+            allowNull: false,
+          },
+        },
+        { transaction: t },
+      )
+
+      await queryInterface.createTable(
+        'income_lines',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          income_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: 'income',
+              key: 'id',
+            },
+          },
+          income_type_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: 'income_types',
+              key: 'id',
+            },
+          },
+          label: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          value: {
+            type: Sequelize.FLOAT,
+            allowNull: false,
+          },
+          currency: {
+            type: Sequelize.STRING,
+            defaultValue: 'ISK',
+          },
+        },
+        { transaction: t },
+      )
+    })
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.dropTable('tax_return', { transaction: t })
+      await queryInterface.dropTable('th_people', { transaction: t })
+      await queryInterface.dropTable('th_address', { transaction: t })
+      await queryInterface.dropTable('income_lines', { transaction: t })
+      await queryInterface.dropTable('income', { transaction: t })
+      await queryInterface.dropTable('income_types', { transaction: t })
+    })
   },
 }
