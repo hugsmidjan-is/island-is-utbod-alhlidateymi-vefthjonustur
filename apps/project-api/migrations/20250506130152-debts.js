1edd6141fbb5c5c87932c5eb8eda4e9c1d3d9da0
Kári Yngvason <kari@hugsmidjan.is>
2025-05-06 16:13:06 +0000

    feat: add debt database schema

---
+++ apps/project-api/migrations/20250506130152-debts.js
@@ -0,0 +1,147 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.createTable(
+        'debt_types',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+        },
+        {
+          transaction: t,
+        },
+      )
+
+      await queryInterface.createTable(
+        'debt',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          created: {
+            type: 'TIMESTAMP WITH TIME ZONE',
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            allowNull: false,
+          },
+          modified: {
+            type: 'TIMESTAMP WITH TIME ZONE',
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            allowNull: false,
+          },
+          type: {
+            type: Sequelize.ENUM('prefill', 'submit'),
+            allowNull: false,
+          },
+          debt_type_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: 'debt_types',
+              key: 'id',
+            },
+          },
+          tax_return_id: {
+            type: Sequelize.UUID,
+            references: {
+              model: 'tax_return',
+              key: 'id',
+            },
+          },
+        },
+        { transaction: t },
+      )
+
+      await queryInterface.createTable(
+        'debt_lines',
+        {
+          id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          debt_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: 'debt',
+              key: 'id',
+            },
+          },
+          debt_type_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: 'debt_types',
+              key: 'id',
+            },
+          },
+          term: {
+            type: Sequelize.INTEGER,
+          },
+          origination_date: {
+            type: Sequelize.DATEONLY,
+          },
+          identifier: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          label: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          outstanding_principal: {
+            type: Sequelize.FLOAT,
+            allowNull: false,
+          },
+          interest_amount: {
+            type: Sequelize.FLOAT,
+            allowNull: false,
+          },
+          annual_total_payment: {
+            type: Sequelize.FLOAT,
+            allowNull: false,
+          },
+          annual_total_principal_payment: {
+            type: Sequelize.FLOAT,
+          },
+          creditor_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'th_people',
+              key: 'national_id',
+            },
+          },
+          currency: {
+            type: Sequelize.STRING,
+            defaultValue: 'ISK',
+          },
+        },
+        { transaction: t },
+      )
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.dropTable('debt_lines', { transaction: t })
+      await queryInterface.dropTable('debt', { transaction: t })
+      await queryInterface.dropTable('debt_types', { transaction: t })
+    })
+  },
+}
